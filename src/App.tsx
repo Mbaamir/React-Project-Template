f140943f72ec7c6cff41296d621f62e893d2d851
Muhammad Bin Aamir <MS.bin.aamir@gmail.com>
2022-05-25 22:28:44 +0500

    Clean Up

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import SignIn from "./Pages/SignIn/SignIn";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Create React App With</h1>
+      <ul>
+        <li>React@18.1.0</li>
+        <li>Typescript</li>
+        <li>Material UI With Styled Components Styling Engine</li>
+        <li>Styled Components </li>
+      </ul>
     </div>
   );
 }
